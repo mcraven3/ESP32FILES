7becb7cea60dc45a19fa6fb600f3c463398eff4e
mcraven3 <matthew.p.craven@gmail.com>
2024-06-11 17:00:12 -0400

    Update SGP40codeFINISHED.py

--- SGP40codeFINISHED.py
+++ SGP40codeFINISHED.py
@@ -10,7 +10,7 @@ pixel = neopixel.NeoPixel(board.NEOPIXEL, 1) #shortens neopixel command, can be 
 i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
 try: #try statement to catch errors
-    with open("/SGP40_data_values.txt", "a") as gas_log: #open text file, in appendage mode, called gas_log
+    with open("/SGP40_data_values.csv", "a") as gas_log: #open text file, in appendage mode, called gas_log
         while True:
             sgp = adafruit_sgp40.SGP40(i2c) #pull data from sensor, storing as variable
             gas_log.write("raw gas:  ") #begin writing in file
